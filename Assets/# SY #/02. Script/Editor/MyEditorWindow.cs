af89737cf48c34436c4650beede6095a5a7667eb
Siyeol Lee <siyel4512@naver.com>
2022-07-03 13:47:15 +0900

    Editor Window Create1

--- Assets/# SY #/02. Script/Editor/MyEditorWindow.cs
+++ Assets/# SY #/02. Script/Editor/MyEditorWindow.cs
@@ -5,6 +5,48 @@ using UnityEditor; // unity editor 관련 api를 사용하기위해 선언
 
 public class MyEditorWindow : EditorWindow
 {
+    #region EditorWindow 다중 생성 방법1
+
+    //static MyEditorWindow window;
+
+    //[MenuItem("MyTool/Open MyTool %g")]
+    //static void Open()
+    //{
+    //    Debug.Log("My Tool 창 열림");
+
+    //    window = CreateInstance<MyEditorWindow>(); // EditorWindow가 생성됨
+
+    //    window.Show();
+    //}
+
+    #endregion
+
+
+
+    #region 1개의 EditorWindow 생성 방법1
+
+    //static MyEditorWindow window;
+
+    //[MenuItem("MyTool/Open MyTool %g")]
+    //static void Open()
+    //{
+    //    Debug.Log("My Tool 창 열림");
+
+    //     다중 생성을 막으려면 현재 EditorWindow가 생성되었는지 확인
+    //    if (window == null)
+    //    {
+    //        window = CreateInstance<MyEditorWindow>();
+    //    }
+
+    //    window.Show();
+    //}
+
+    #endregion
+
+
+
+    #region 1개의 EditorWindow 생성 방법2
+
     // 만든 editor window를 오픈을 해줄 함수 선언, static 선언
     // unity editor의 메뉴에서 메뉴창을 선택하면 해당 함수가 실행(호출된다.)
     //MenuItem에서 %g는 단축키를 의미하며 crtl + g를 의미한다.
@@ -14,8 +56,12 @@ public class MyEditorWindow : EditorWindow
     {
         Debug.Log("My Tool 창 열림");
 
-        // window 창 생성
+        // EditorWindow 생성 (한개만 생성됨
         var window = GetWindow<MyEditorWindow>();
+        
+        // EditorWindow 이름 설정 (설정안하면 default 값으로 Editor Window가 된다.)
         window.title = "My Tool";
     }
+    #endregion
+
 }
